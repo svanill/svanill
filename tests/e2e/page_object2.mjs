1f2ecfbbf71ae584d7fe9ce930e886ebecca5af3
Riccardo Attilio Galli <riccardo@sideralis.org>
2022-06-24 21:44:09 -0700

    Rewrite network tests using playwright

---
+++ tests/e2e/page_object2.mjs
@@ -0,0 +1,76 @@
+// @ts-check
+
+export default class VanillaCryptoPage {
+  /**
+   * @param {import('@playwright/test').Page} page
+   */
+  constructor(page) {
+    this.page = page;
+  }
+  getInputSecret() {
+    return this.page.locator('#secret');
+  }
+  getInputConfirmSecret() {
+    return this.page.locator('#confirm-secret');
+  }
+  getInputUsername() {
+    return this.page.locator('#username');
+  }
+  getLoginButton() {
+    return this.page.locator('#login-btn');
+  }
+  getErrorBar() {
+    return this.page.locator('#error-bar');
+  }
+  getLoginDialog() {
+    return this.page.locator('#login-form');
+  }
+  getMainContainer() {
+    return this.page.locator('.main-container');
+  }
+  getReloadCiphertextButton() {
+    return this.page.locator('#reload-ciphertext');
+  }
+  getUploadCiphertextButton() {
+    return this.page.locator('#upload-ciphertext');
+  }
+  async login() {
+    await this.getInputSecret().fill('');
+    await this.getInputSecret().type('testpw');
+
+    await this.getInputConfirmSecret().fill('');
+    await this.getInputConfirmSecret().type('testpw');
+
+    await this.getLoginButton().click();
+  }
+  async loginExt(username) {
+    await this.getInputSecret().fill('');
+    await this.getInputSecret().type('testpw');
+
+    await this.getInputConfirmSecret().fill('');
+    await this.getInputConfirmSecret().type('testpw');
+
+    await this.getInputUsername().fill('');
+    await this.getInputUsername().type(username || 'test-user');
+
+    await this.getLoginButton().click();
+  }
+  async loginUsingKeyboard() {
+    await this.getInputSecret().fill('');
+    await this.getInputSecret().type('testpw');
+
+    await this.getInputConfirmSecret().fill('');
+    await this.getInputConfirmSecret().type('testpw');
+
+    await this.getLoginButton().press('Enter');
+  }
+  getCleartextTextarea() {
+    return this.page.locator('#plaintext');
+  }
+  getCiphertextTextarea() {
+    return this.page.locator('#ciphertext');
+  }
+  getCiphertextErrorBar() {
+    return this.page.locator('#ciphertext-error-bar');
+  }
+}
