9833491943d1b30dd2224a213e7aabb397577663
Riccardo Attilio Galli <riccardo@sideralis.org>
2024-08-03 10:26:13 -0700

    Use docker compose instead of docker-compose

--- bin/run_tests_e2e.sh
+++ bin/run_tests_e2e.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 command -v node && node --version
-command -v docker-compose && docker-compose --version
+command -v docker && docker --version
 
 cd $DIR/..
 ROOT=$PWD
@@ -14,15 +14,15 @@ npx playwright test --browser chromium e2e/networkless.spec.mjs
 npx playwright test --browser firefox e2e/networkless.spec.mjs
 
 # On exit, teardown the docker containers
-trap "docker-compose -f ${ROOT}/tests/e2e/docker-compose.yml down" EXIT
+trap "docker compose -f ${ROOT}/tests/e2e/docker-compose.yml down" EXIT
 
 # Setup the docker containers
-docker-compose -f tests/e2e/docker-compose.yml up -d
+docker compose -f tests/e2e/docker-compose.yml up --force-recreate -d
 
 # reset (empty) s3 (minio) so tests run from a blank state
-docker-compose -f tests/e2e/docker-compose.yml up --force-recreate -d createbuckets
+docker compose -f tests/e2e/docker-compose.yml up --force-recreate -d createbuckets
 npx playwright test --browser chromium e2e/network_on.spec.mjs
 
 # reset (empty) s3 (minio) so tests run from a blank state
-docker-compose -f tests/e2e/docker-compose.yml up --force-recreate  -d createbuckets
+docker compose -f tests/e2e/docker-compose.yml up --force-recreate  -d createbuckets
 npx playwright test --browser firefox e2e/network_on.spec.mjs
